11e6fef38d39c13ad5b7f714c996071520a09dc1
nongsang <shdtkd37@gmail.com>
2017-03-13 00:23:40 +0900

    3월 13일 클래스 템플릿 특수화

---
+++ 64.class_template_specialization.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class CTest
+{
+	T m_Data;
+public:
+	CTest(T param) : m_Data(param) { }
+
+	T GetData() const { return m_Data; }
+	void SetData(T param) { m_Data = param; }
+};
+
+template<>		// 여기서부터 템플릿 특수화를 사용해준다는 의미
+class CTest<char*>		// 형태를 잘 보렴
+{
+	char *m_pData;
+public:
+	CTest(char *pszParam)
+	{
+		int nLen = strlen(pszParam);
+		m_pData = new char[nLen + 1];
+		strcpy_s(m_pData, nLen + 1, pszParam);
+	}
+	~CTest() { delete[] m_pData; }
+
+	char* GetData() const { return m_pData; }
+};
+
+int main()
+{
+	CTest<char*> a("Hello");		// 클래스 템플릿 특수화 사용
+	cout << a.GetData() << endl;
+}
+
+// 4 ~ 13번 줄
+// 일반적인 템플릿 클래스를 정의해주었다.
+
+// 15번 줄
+// 이제부터 템플릿 특수화를 사용하겠다는 의미
+
+// 16번 줄
+// 클래스 템플릿 특수화를 사용하는 방법이다.
+// 클래스 이름 옆에 특수하게 사용할 자료형을 붙여준다.
+
+// 33번 줄
+// 클래스 템플릿 특수화로 정의한 클래스가 호출된다.
+// 기본적으로 쓰이는 템플릿을 정의해도 어차피 같은 기능을 하는데 굳이 특수화를 써야하나?
+
+// 65.template_specialization_notgood에서 다뤄보자
